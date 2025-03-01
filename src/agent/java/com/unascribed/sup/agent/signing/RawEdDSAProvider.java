4e0877aeb34712118594d3a46925ad3a8788a692
Una Kearney <me@unascribed.com>
2025-03-01 16:33:18 -0500

    Fix license

--- src/agent/java/com/unascribed/sup/agent/signing/RawEdDSAProvider.java
+++ src/agent/java/com/unascribed/sup/agent/signing/RawEdDSAProvider.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of unsup.
+ * Copyright © 2020-2025 Una Kearney (unascribed) and contributors
+ * https://git.sleeping.town/unascribed/unsup
+ *
+ * unsup is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * unsup is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with unsup; if not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.unascribed.sup.agent.signing;
 
 import java.security.InvalidAlgorithmParameterException;
