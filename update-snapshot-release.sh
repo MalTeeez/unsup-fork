c86e34fd1e589b869c49ebbe7588a2cfc9d5145c
Una Kearney <me@unascribed.com>
2025-05-28 17:01:54 -0400

    Rework CI snapshots for component update support

--- update-snapshot-release.sh
+++ update-snapshot-release.sh
@@ -17,38 +17,13 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with unsup; if not, see <https://www.gnu.org/licenses/>.
 #
-
-latest=$(curl -fs 'https://repo-api.sleeping.town/com/unascribed/unsup/maven-metadata.xml' |xq -r '.metadata.versioning.latest')
-snap=
-snapurl=
-if [[ "$latest" =~ -SNAPSHOT$ ]]; then
-	snap=$(curl -fs "https://repo-api.sleeping.town/com/unascribed/unsup/$latest/maven-metadata.xml" |xq -r '.metadata.versioning.snapshot|"\(.timestamp)-\(.buildNumber)"')
-	snapurl="https://repo.sleeping.town/com/unascribed/unsup/$latest/unsup-${latest%-SNAPSHOT}-$snap"
-	cat > com.unascribed.unsup.json <<EOF
-{
-	"formatVersion": 1,
-	"name": "unsup",
-	"uid": "com.unascribed.unsup",
-	"version": "$latest-$snap",
-	"+agents": [
-		{
-			"name": "com.unascribed:unsup:$latest-$snap",
-			"MMC-absoluteUrl": "$snapurl.jar"
-		}
-	]
-}
-EOF
-else
-	exit 1
-fi
-
-
+latest=$(cat ci-version.txt)
 apibase='https://git.sleeping.town/api/v1/repos/unascribed/unsup'
 auth="Authorization: token $FORGEJO_KEY"
 curl -s -X DELETE -H "$auth" $apibase/releases/tags/SNAPSHOT >/dev/null
 curl -s -X DELETE -H "$auth" $apibase/tags/SNAPSHOT >/dev/null
 
-lasttag=$(curl -s "$apibase/releases?draft=false&pre-release=true&page=1&limit=1" |jq -r '.[0].tag_name')
+lasttag=$(curl -s "$apibase/releases?draft=false&page=1&limit=1" |jq -r '.[0].tag_name')
 
 resp=$(curl -s -X POST -H 'Content-Type: application/json' -H "$auth" $apibase/releases --data-raw "
 {
@@ -69,13 +44,13 @@ fi
 
 echo 'Jar attach response:'
 curl --fail-with-body -s -X POST -H "$auth" "$apibase/releases/$relid/assets?name=unsup-$latest.jar" \
-	-F "external_url=$snapurl.jar" |jq .
+	-F "attachment=@build/libs/unsup-$latest.jar" |jq .
 echo 'Sig attach response:'
 curl --fail-with-body -s -X POST -H "$auth" "$apibase/releases/$relid/assets?name=unsup-$latest.jar.sig" \
-	-F "external_url=$snapurl.jar.sig" |jq .
+	-F "attachment=@build/libs/unsup-$latest.jar.sig" |jq .
 echo 'Component attach response:'
 curl --fail-with-body -s -X POST -H "$auth" "$apibase/releases/$relid/assets?name=com.unascribed.unsup.json" \
-	-F "attachment=@com.unascribed.unsup.json" |jq .
+	-F "attachment=@build/libs/com.unascribed.unsup.json" |jq .
 
 echo 'Undraft response:'
 curl --fail-with-body -s -X PATCH -H 'Content-Type: application/json' -H "$auth" "$apibase/releases/$relid" \
